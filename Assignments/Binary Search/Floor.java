d5e10c9b5f5303f954e4a057f9853da29eb5041c
Dhruv Sharma <dhruvsharma@192.168.1.5>
2025-10-24 14:34:57 +0530

    Working on Binary Search Questions

---
+++ Assignments/Binary Search/Floor.java
@@ -0,0 +1,24 @@
+public class Floor {
+    public static void main(String[] args) {
+        int[] arr= {22,25,45,69,85,255};
+        System.out.print(Floor(arr, 69));
+    }
+     static int Floor(int[] arr, int target){
+        int start=0;
+        int end=arr.length - 1;
+        while (start<=end) { 
+            //Find the middle element
+            int mid = start + (end-start) / 2;
+            if(target<arr[mid]){
+                end= mid-1;
+            }else if(target>arr[mid]){
+                start= mid +1;
+            }
+            else{
+                //Answer Found
+                return  mid;
+            }
+        }
+        return end;
+    }
+}
---
+++ Assignments/Binary Search/LectureQuestions/CeillingOfANumber.java
@@ -0,0 +1,29 @@
+
+public class CeillingOfANumber {
+    public static void main(String[] args) {
+         int[] arr= {22,25,45,69,85,255};
+        System.out.print(CeillingbinarySearch(arr, 69));
+        //Ceilling = smallest element in array greater or equal to target element
+    }
+    //Return -1 if target not exists.
+    static int CeillingbinarySearch(int[] arr, int target){
+        int start=0;
+        int end=arr.length - 1;
+        while (start<=end) { 
+            //Find the middle element
+            int mid = start + (end-start) / 2;
+            if(target<arr[mid]){
+                end= mid-1;
+            }else if(target>arr[mid]){
+                start= mid +1;
+            }
+            else{
+                //Answer Found
+                return  mid;
+            }
+        }
+        return start;
+    }
+}
+
+
