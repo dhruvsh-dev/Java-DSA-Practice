11a3f2a2a6914e94f9996b94a86b5df51107a840
Dhruv Sharma <dhruvsharma@192.168.1.20>
2025-11-18 10:53:20 +0530

    Selection Sort

---
+++ Assignments/Sorting/SelectionSort.java
@@ -0,0 +1,35 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+
+        selectionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = maxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int maxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
