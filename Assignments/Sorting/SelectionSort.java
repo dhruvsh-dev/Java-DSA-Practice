dc78209ee1ddec420218a4d3c469f58687377b98
Dhruv Sharma <dhruvshdev@gmail.com>
2025-11-21 22:45:43 +0530

    Update SelectionSort.java

--- Assignments/Sorting/SelectionSort.java
+++ Assignments/Sorting/SelectionSort.java
@@ -13,7 +13,7 @@ public class SelectionSort {
         for (int i = 0; i < arr.length; i++) {
             int last = arr.length - i - 1;
             int maxIndex = maxIndex(arr, 0, last);
-            swap(arr, maxIndex, last);
+            swap(arr, maxIndex, last); 
         } 
     }
 
