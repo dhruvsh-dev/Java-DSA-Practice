59a651c5fa5952ca6502491f4ae4889608726bf1
Dhruv Sharma <dhruvshdev@gmail.com>
2025-11-23 21:12:53 +0530

    Update SpiralMatrix.java

--- Assignments/Arrays/Medium/SpiralMatrix.java
+++ Assignments/Arrays/Medium/SpiralMatrix.java
@@ -7,7 +7,7 @@ public class SpiralMatrix {
 
         if (matrix == null || matrix.length == 0) return result;
 
-        int top = 0;
+        int top = 0; 
         int bottom = matrix.length - 1;
         int left = 0;
         int right = matrix[0].length - 1;
