9f6b1146d2ad05a012fc7d87dd2a71a983f50339
Dhruv Sharma <dhruvshdev@gmail.com>
2025-11-24 23:03:01 +0530

    Update BuildArrayFromPermutation.java

--- Assignments/Arrays/easy/BuildArrayFromPermutation.java
+++ Assignments/Arrays/easy/BuildArrayFromPermutation.java
@@ -15,7 +15,7 @@ public class BuildArrayFromPermutation {
         for (int i = 0; i < n; i++) {
             nums[i] = nums[i] + (nums[nums[i]] % n) * n;
         }
-
+ 
         // decode to final values
         for (int i = 0; i < n; i++) {
             nums[i] = nums[i] / n;
