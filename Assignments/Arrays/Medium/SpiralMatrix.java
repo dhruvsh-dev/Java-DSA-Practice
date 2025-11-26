5c81d55da99a98cc2ca0d34ad31baeef14471c47
Dhruv Sharma <dhruvshdev@gmail.com>
2025-11-26 22:31:22 +0530

    Update SpiralMatrix.java

--- Assignments/Arrays/Medium/SpiralMatrix.java
+++ Assignments/Arrays/Medium/SpiralMatrix.java
@@ -10,7 +10,7 @@ public class SpiralMatrix {
         int top = 0; 
         int bottom = matrix.length - 1;
         int left = 0;
-        int right = matrix[0].length - 1;
+        int right = matrix[0].length - 1; 
 
         while (top <= bottom && left <= right) {
  
