58fa192cd06903a59786b1102cecafd8f32b722b
Dhruv Sharma <dhruvshdev@gmail.com>
2026-03-09 14:53:39 +0530

    Create Armstrong Numbers.java

---
+++ Extra Questions/Armstrong Numbers.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    static boolean isArmstrong(int num) {
+
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        int num = 153;
+
+        if (isArmstrong(num))
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
