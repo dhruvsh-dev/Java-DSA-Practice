dedf1ecce67cd55f99d25e815043c82bfa0bbc35
Dhruv Sharma <dhruvshdev@gmail.com>
2026-03-10 17:55:26 +0530

    Create Selection Sort.java

---
+++ Extra Questions/Sorting/Selection Sort.java
@@ -0,0 +1,18 @@
+public static void selectionSort(int[] arr) {
+    int n = arr.length;
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // swap
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
